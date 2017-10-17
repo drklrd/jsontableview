3be52b15ae5600fa4e4bf64059c85aab21145d7a
drklrd <srvbhattarai@gmail.com>
2017-10-17 21:19:09 +0545

    add header component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
-      <div className="row">
-        <div className="col-6">
-            <h1>Part 1</h1>
+        <div>
+            <Header/>
+            <div className="row">
+              <div className="col-6">
+                  <h1>Part 1</h1>
+              </div>
+              <div className="col-6">
+                  <h1>Part 2</h1>
+              </div>
+            </div>
         </div>
-        <div className="col-6">
-            <h1>Part 2</h1>
-        </div>
-      </div>
     );
   }
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React , { Component } from 'react';
+import './css/header.css';
+
+export default class Header extends Component{
+    render(){
+        return (
+            <div className="header">
+                <h3>JSON Table view</h3>
+            </div>
+        );
+    }
+}
